b281a49906454e45428b6fa8b3b9ed4fcf1e0e45
Theo Code <115007685+TheoSauval@users.noreply.github.com>
2025-03-31 10:14:56 +0200

    Initial Commit

---
+++ E-commerceShop/E_commerceShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerceShopApp.swift
+//  E-commerceShop
+//
+//  Created by Théo Sauval on 31/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct E_commerceShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ E-commerceShopTests/E_commerceShopTests.swift
@@ -0,0 +1,16 @@
+//
+//  E_commerceShopTests.swift
+//  E-commerceShopTests
+//
+//  Created by Théo Sauval on 31/03/2025.
+//
+
+import Testing
+
+struct E_commerceShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
